2ef3f6ca039907ed86472347cbb07c3b8c4e7f93
ruben <molinaescabiasr@gmail.com>
2026-06-04 17:32:47 +0200

    config

---
+++ backend/gateway/src/main/java/com/example/api_gateway/config/CorsConfig.java
@@ -0,0 +1,38 @@
+package com.example.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.setAllowedOrigins(
+                List.of("https://sistema-logistica-militar.netlify.app")
+        );
+
+        config.setAllowedMethods(
+                List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+        );
+
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
--- backend/gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
+++ backend/gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -18,7 +19,7 @@ public class SecurityConfig {
 
                 .authorizeExchange(exchanges -> exchanges
 
-                        .pathMatchers(org.springframework.http.HttpMethod.OPTIONS)
+                        .pathMatchers(HttpMethod.OPTIONS)
                         .permitAll()
 
                         .pathMatchers(
